2dab48901fb7279b10dd1cb1471eaf15927b294f
Steve <contact@stevedylan.dev>
2026-01-26 22:08:17 -0500

    chore: updated favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { onNavigate } from "$app/navigation";
   import "./layout.css";
-  import favicon from "$lib/assets/favicon.svg";
 
   let { children } = $props();
 
@@ -18,7 +17,7 @@
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
+  <link rel="icon" href="/favicon.ico" />
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
