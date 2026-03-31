4452d33ac81d908db620d702f006fb259641a231
Steve <contact@stevedylan.dev>
2026-03-31 19:35:18 -0400

    chore: update og and icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,6 +21,7 @@ onNavigate((navigation) => {
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <link rel="manifest" href="/site.webmanifest">
   <link rel="alternate" type="application/rss+xml" title="RSS" href="https://steve.photo/rss.xml" />
   <meta name="description" content="Photography by Steve" />
   <meta property="og:site_name" content="steve.photo" />
